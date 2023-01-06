17fa2cd2a1bcaee254be8943d3ce4a5b51ac689f
kangzy-fix <kangzy.fix@student.moringaschool.com>
2023-01-06 16:48:11 +0300

    create the header componets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
 
                 <div>
                     <ul className='flex space-x-10'>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500" }`} onClick={()=>navigate("/")} >Home</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500" }` onClick={()=>navigate("/")} >Home</li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500" }`} onClick={()=>navigate("/offers")}  >Offers</li>
                         <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500" }`} onClick={()=>navigate("/sign-in")}  >Sign In</li>
 
