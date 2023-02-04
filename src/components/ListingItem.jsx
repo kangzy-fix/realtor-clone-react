4365319931741c27c2f3f849da2394f8134972a3
ian kangacha <kangzyfix@gmail.com>
2023-02-04 17:46:21 +0300

    Added functionality in listing item file

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -11,7 +11,7 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
         <img
           className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
           loading="lazy"
-          src={listing.imgUrls[0]}
+          src={listing.imgUrls[0]} alt="Loading..."
         />
         <Moment
           className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
